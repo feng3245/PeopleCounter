5520d42d2198198d60c437cbe687a4f1a6fde763
root <feng3245@gmail.com>
2020-04-27 00:45:58 +0000

    Have determined effort for having main script to be run in jupyter is too great. Will have only pre-steps run within jupyter

--- inference.py
+++ inference.py
@@ -36,7 +36,7 @@ class Network:
 
     def __init__(self):
         ### TODO: Initialize any class variables desired ###
-
+        return
     def load_model(self):
         ### TODO: Load the model ###
         ### TODO: Check for supported layers ###
